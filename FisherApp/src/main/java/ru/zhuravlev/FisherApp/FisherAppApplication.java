63872512f5b76ce64c34c9685447f22a926318ff
bigbrooogo <serko107@gmail.com>
2025-07-18 12:07:52 +0300

    Initial project

---
+++ FisherApp/src/main/java/ru/zhuravlev/FisherApp/FisherAppApplication.java
@@ -0,0 +1,13 @@
+package ru.zhuravlev.FisherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisherAppApplication.class, args);
+	}
+
+}
---
+++ FisherApp/src/test/java/ru/zhuravlev/FisherApp/FisherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zhuravlev.FisherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
